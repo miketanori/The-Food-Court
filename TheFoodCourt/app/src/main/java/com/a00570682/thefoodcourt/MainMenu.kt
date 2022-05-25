0d72c72ea0a3b2ec52e27e906975c59eb0fbfeaa
BernardoG <A00570682@itesm.mx>
2022-05-25 18:44:27 -0500

    Added food card item

--- TheFoodCourt/app/src/main/java/com/a00570682/thefoodcourt/MainMenu.kt
+++ TheFoodCourt/app/src/main/java/com/a00570682/thefoodcourt/MainMenu.kt
@@ -1,21 +1,16 @@
 package com.a00570682.thefoodcourt
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 //import androidx.navigation.fragment.NavHostFragment
 //import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.a00570682.thefoodcourt.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainMenu : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnLogin.setOnClickListener {
-
-        }
+        setContentView(R.layout.main_scrollable)
     }
 }
